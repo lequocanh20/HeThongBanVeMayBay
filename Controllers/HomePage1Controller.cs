4405b501a7ac9d32f0b32c8dd1dd1bab190f8db9
Lê Quốc Anh <66353897+lequocanh20@users.noreply.github.com>
2021-06-19 21:07:20 +0700

    thêm giao diện cho người dùng

---
+++ Controllers/HomePage1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeThongBanVeMayBay.Controllers
+{
+    public class HomePage1Controller : Controller
+    {
+        // GET: HomePage1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
